1171a1fc453afcf3d0786fa9d98ba4cdf0324c3d
Sebastian Castro <sebastian@nzrs.net.nz>
2015-02-22 19:27:02 +1300

    Finishing the implementation

--- server.R
+++ server.R
@@ -1,7 +1,36 @@
 library(shiny)
-library(ggplot)
+library(ggplot2)
+library(reshape)
+csv_file <- "data/ip5-macondo.csv"
+v5_data <- read.csv(csv_file)
+v5_data$Month <- as.Date(paste(v5_data$Month, "01", sep="-"))
+totals <- v5_data[ v5_data$metric == 'total_domain', c('Month', 'counter')]
+names(totals) <- c('Month', 'total')
+v5_data <- merge(v5_data, totals, by='Month')
+
 shinyServer(
   function(input, output) {
-    
+    clean_v5 <- reactive({
+        types <- unlist(strsplit(as.character(input$types), " "))
+        clean_v5 <- v5_data[ v5_data$metric %in% types, ]
+        if (input$perc) {
+          clean_v5$counter <- 100*(clean_v5$counter/clean_v5$total)
+        }
+        clean_v5
+    })
+    output$deployment_plot <- renderPlot({
+      if (input$perc) {
+        y_label <- "Percentage of Domains"
+      } else {
+        y_label <- "Number of Domains"
+      }
+      base_plot <-  ggplot(clean_v5(), aes(x=Month, y=counter, color=metric)) + geom_line(size=1.0) + theme_bw() + labs(x="", y=y_label, title="V5 deployment") + scale_color_discrete(name="Service")
+      if (input$lm) {
+        base_plot + geom_smooth(method="lm")
+      } else {
+        base_plot
+      }
+    })
   }
 )
+# runApp("~/Documents/personal/Coursera/Developing.Data.Products/Project")
--- ui.R
+++ ui.R
@@ -2,6 +2,16 @@ library(shiny)
 shinyUI(pageWithSidebar(
   headerPanel("IPv5 adoption in Macondo"),
   sidebarPanel(
-    checkboxGroupInput("types", "Type of Service", c("Web" = 1, "Mail" = 2, "DNS" = 3))),
-  mainPanel(h4('Main Panel'))
+    checkboxGroupInput("types", "Type of Service", c("Web" = "Web", "Mail" = "Mail", "DNS" = "DNS")),
+    h4("Modifiers"),
+    checkboxInput('perc', label = 'Display as Percentage?', value=FALSE),
+    checkboxInput('lm', label = 'Include Linear Regression?', value=FALSE),
+    h3("Description"),
+    p("This application allows you to explore the status of deployment of IPv5 in Macondo, a magical city introduced by Gabriel Garcia Marques in the book \"A Hundred Years of Solitude\"."),
+    p("IPv5, as being an advanced technology, is of great interest for the Buendia family, so they run a monthly survey where they ask each domain holder in the city if they have IPv5 service for their domain, website and mail server. That data is presented here."),
+    h3("Instructions"),
+    div("Select any combination of service (Web, Mail, DNS) to start. The modifiers allow you to explore the data in different ways."), strong("Display as percentage"), div(" normalizes the data by the total number of domains, useful to identify if there is any growth. "), strong("Include Linear regression"), div(" overlaps the active plot with the calculated linear regression and confidence interval")  ),
+  mainPanel(
+    textOutput('type_of_service'),
+    plotOutput('deployment_plot'))
   ))
